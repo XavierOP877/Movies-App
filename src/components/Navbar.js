0613816f42f4a5b8f0cd50eed3f83da9842eeb20
XavierOP877 <tshivank620@gmail.com>
2022-08-06 21:04:45 +0530

    movies app completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+
+class Navbar extends Component{
+    render(){
+        return(
+            <div style={{display:"flex",padding:'0.5rem',color:"blue"}}>
+                <Link to="/" style={{textDecoration:"none"}} ><h1>Movies App</h1></Link>
+                <Link to="/favourites" style={{textDecoration:"none"}}><h2 style={{marginLeft:"2rem",marginTop:"0.5rem"}}>Favourites</h2></Link>
+            </div>
+        )
+    }
+}
+
+export default Navbar
+
